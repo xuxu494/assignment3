6c6d3584919ab94dcfcac51e9ac87df9f901bcf8
xuxu494 <xx494@nyu.edu>
2015-09-28 22:55:18 -0400

    Change-Id: Ic8acf39a393079e60737e89568e9ae4fea681bc4 Signed-off-by: Xu Xu <xx494@nyu.edu>

---
+++ xx494/my_package/hello_world.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 28, 2015
+
+@author: ds-ga-1007
+
+def hello_world():
+    print "Hello World!"
+
+
+
+'''
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    hello_world()
